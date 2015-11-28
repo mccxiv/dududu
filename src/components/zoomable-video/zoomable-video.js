7a7fea9b157ab08dd6c896c4c7668ac596716e09
Andrea Stella <adr.stella@gmail.com>
2015-11-28 14:58:58 -0600

    Push initial files, lots to do still

---
+++ src/components/zoomable-video/zoomable-video.js
@@ -0,0 +1,50 @@
+class ZoomableVideo extends React.Component {
+	componentDidMount() {
+		var el = $(ReactDOM.findDOMNode(this));
+
+		var v = el.find('.video');
+		var vc = el.find('.video-container');
+		var vcc = el;
+
+		vc.panzoom({
+			contain: 'invert',
+			minScale: 1,
+			increment: 0.8,
+			easing: "ease-in-out"
+		});
+
+		vcc.on('mousewheel.focal', function( e ) {
+			e.preventDefault();
+			var delta = e.delta || e.originalEvent.wheelDelta;
+			var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+			vc.panzoom('zoom', zoomOut, {focal: e});
+		});
+
+		vcc.on('mousewheel', function() {
+			console.log('wheel.');
+			setTimeout(checkZoomLevel, 200);   // Check initial
+			setTimeout(checkZoomLevel, 1500);  // Check final state
+		});
+
+		function checkZoomLevel() {
+			if (v[0].getBoundingClientRect().width > vc.width()) {
+				v.css('z-index', '-1');
+			}
+			else v.css('z-index', 'unset');
+		}
+	}
+
+	render() {
+		return (
+			<div className="video-container-container">
+				<div className="video-container">
+					<iframe className="video"
+							frameBorder="0"
+							allowFullScreen
+							src="http://player.twitch.tv/?channel={this.props.channel}&html5"
+					></iframe>
+				</div>
+			</div>
+		);
+	}
+}

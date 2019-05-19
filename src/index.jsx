29d04f346225ae924f8b07591ef3ad1fafc8d3b2
Mccxiv <adr.stella@gmail.com>
2019-05-19 11:02:11 +0800

    Clean up

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/home.jsx';
-import Channel from './components/channel.jsx';
-import {Router, Route} from 'react-router';
-import {createHistory} from 'history';
 import '../node_modules/react-mdl/extra/material.js';
 import '!!style!css!../node_modules/react-mdl/extra/material.css';
 import '!!style!css!./assets/styles.css';
-
-const history = createHistory({
-	queryKey: false
-});
 
 ReactDOM.render(
 	<Home />,
